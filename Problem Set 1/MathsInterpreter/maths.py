90b17a97ecf95bce9604fa10231e9d82fa57e44b
Ebube Okafor <okaforebube8@gmail.com>
2023-05-29 23:47:03 +0100

    Update maths.py

--- Problem Set 1/MathsInterpreter/maths.py
+++ Problem Set 1/MathsInterpreter/maths.py
@@ -1,28 +1,28 @@
 maths = input("Enter a basic arithmetic expression: ")
 maths = maths.split()
 if maths[1] == "/":
-    maths[0] = int(maths[0])
-    maths[2] = int(maths[2])
+    maths[0] = float(maths[0])
+    maths[2] = float(maths[2])
     expression = maths[0] / maths[2]
     print(round(expression, 1))
 elif maths[1] == "-":
-    maths[0] = int(maths[0])
-    maths[2] = int(maths[2])
+    maths[0] = float(maths[0])
+    maths[2] = float(maths[2])
     expression = maths[0] - maths[2]
-    print(f"{expression, .1f}")
+    print(round(expression, 1))
 elif maths[1] == "+":
-    maths[0] = int(maths[0])
-    maths[2] = int(maths[2])
+    maths[0] = float(maths[0])
+    maths[2] = float(maths[2])
     expression = maths[0] + maths[2]
     print(round(expression, 1))
 elif maths[1] == "*":
-    maths[0] = int(maths[0])
-    maths[2] = int(maths[2])
+    maths[0] = float(maths[0])
+    maths[2] = float(maths[2])
     expression = maths[0] * maths[2]
     print(round(expression, 1))
 elif maths[1] == "%":
-    maths[0] = int(maths[0])
-    maths[2] = int(maths[2])
+    maths[0] = float(maths[0])
+    maths[2] = float(maths[2])
     expression = maths[0] % maths[2]
     print(round(expression, 1))
 else:
