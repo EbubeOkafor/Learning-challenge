36441216b71157c441f730f6f10abab08d1981d6
Ebube Okafor <okaforebube8@gmail.com>
2023-06-03 11:55:47 +0100

    Update and rename jd.py to grocery.py

---
+++ Problem Set 3/kdk/grocery.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+grocery_list = defaultdict(int)
+
+while True:
+
+    try:
+
+        item = input("Enter an item for your grocery list (or press Ctrl-D to finish): ")
+
+        # Exit the loop if the user inputs control-d (EOF)
+
+        if not item:
+
+            break
+
+        # Convert the input to uppercase and count the item occurrences
+
+        item = item.upper()
+
+        grocery_list[item] += 1
+
+    except Exception as e:
+
+        print("An error occurred:", str(e))
+
+# Sort the grocery list items alphabetically
+
+sorted_list = sorted(grocery_list.items(), key=lambda x: x[0])
+
+# Display the grocery list with item counts
+
+for item, count in sorted_list:
+
+    print(f"{count} {item}")
