56cbf5ea59eae9d72370aec907ab4719edf3e944
Ebube Okafor <okaforebube8@gmail.com>
2023-06-03 11:32:48 +0100

    Update fuel.py

--- Problem Set 3/Fuel/fuel.py
+++ Problem Set 3/Fuel/fuel.py
@@ -1,20 +1,39 @@
 while True:
+
     try:
-        fraction = input("Enter a fraction: ")
-        x = int(fraction[0])
-        y = int(fraction[2])
-        percent = (x/y)*100
-    except  ZeroDivisionError:
-        print("Cannot divide by 0")
-    else:
-        break
-if x > y:
-    raise ValueError("x should be less than y")
-else:
-    if percent >= 99:
-        print("F")
-    elif percent <= 1:
-        print("E")
-    else:
-        print(percent)
-    
+
+        fraction = input("Enter a fraction in the format x/y: ")
+
+        x, y = map(int, fraction.split('/'))
+
+        if x > y or y == 0:
+
+            raise ValueError("Invalid fraction. Numerator cannot be greater than the denominator, and denominator cannot be zero.")
+
+        percentage = (x / y) * 100
+
+        rounded_percentage = round(percentage)
+
+        if rounded_percentage <= 1:
+
+            grade = 'E'
+
+        elif rounded_percentage >= 99:
+
+            grade = 'F'
+
+        else:
+
+            grade = str(rounded_percentage) + '%'
+
+        print("Grade:", grade)
+
+        break  # Exit the loop if the fraction is valid
+
+    except ValueError as e:
+
+        print("Invalid fraction input:", str(e))
+
+    except Exception as e:
+
+        print("An error occurred:", str(e))
